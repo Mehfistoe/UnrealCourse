0a8e4a879c581186760b236b68fd0981a60f8f78
mehfistoe <mckroc468@outlook.com>
2018-04-14 21:24:52 -0400

    Changed the Requirements doc to txt doc. Got to the 2nd quiz.

--- Section_02/BullsCowsGame/main.cpp
+++ Section_02/BullsCowsGame/main.cpp
@@ -11,26 +11,52 @@ allowing us to use the functions from the included file. */
 // brings std to the global scope so that you don't have to type 'std::'
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
 
-int main() {
+// the entry point of our application
+// You want to keep your main function at the top
+int main()
+{
+
+    PrintIntro();
+   
+    constexpr int NUMBER_OF_TURNS = 5;
+    for(int i = 0; i < NUMBER_OR_TURNS; i++)
+    {
+        GetGuessAndPrintBack();
+        cout << endl;
+    }
+
+    cout << endl;
+	return 0;
+}
+
+// prints the intro of the game
+void PrintIntro()
+{
 	// constexpr cannot be changed
-	constexpr int WORD_LENGTH = 5;
+	constexpr int WORD_LENGTH = 9;
 
 	//std::cout << "Welcome to Bulls and Cows!\n";
 	cout << "Welcome to Bulls and Cows!" << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?" << endl;
+    return;
+}
 
-	// get a guess from the player
-	// guess needs to be one VALID word
-	string player_guess;
-	cout << "Guess the word: ";
-	// you need to use getline because >> splits input on whitespace characters
-	// this register as two guesses
-	getline(cin, player_guess);
+// get and prints a guess from the player
+string GetGuessAndPrintBack()
+{
+    string playerGuess;
+    // get a guess from the player
+    // guess needs to be one VALID word
+    cout << "Guess the word: ";
+    // you need to use getline because >> splits input on whitespace characters
+    // this register as two guesses
+    getline(cin, playerGuess);
 
-	// repeat the guess back to them
-	cout << "Your guess was: " << player_guess << endl;
-
-	return 0;
+    // repeat the guess back to them
+    cout << "Your guess was: " << playerGuess << endl;
+    return playerGuess;
 }
